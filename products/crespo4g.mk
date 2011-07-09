f1901a803847cc704d54ed0664b2e8bc39a325a3
Peter Alfonso <peteralfonsojr@gmail.com>
2011-07-09 11:33:35 -0400

    Update build.prop overrides

--- products/crespo4g.mk
+++ products/crespo4g.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/pete/products/common.mk)
 # Setup device specific product configuration.
 PRODUCT_NAME := pete_crespo4g
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=sojus BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/sojus/crespo4g:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="sojus-user 2.3.4 GRJ22 121341 release-keys" BUILD_NUMBER=121341
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=sojus BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/sojus/crespo4g:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="sojus-user 2.3.4 GRJ22 121341 release-keys" BUILD_NUMBER=121341 BUILD_UTC_DATE=1303507720 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
 
 # Copy crespo4g specific prebuilt files
 PRODUCT_COPY_FILES +=  \
--- products/sholes.mk
+++ products/sholes.mk
@@ -10,7 +10,7 @@ $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage3.mk)
 # Setup device specific product configuration.
 PRODUCT_NAME := pete_sholes
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=voles BUILD_ID=FRG83G BUILD_DISPLAY_ID=GRJ22 TARGET_DEVICE=sholes BUILD_FINGERPRINT=verizon/voles/sholes/sholes:2.2.2/FRG83G/91102:user/release-keys PRODUCT_BRAND=verizon PRIVATE_BUILD_DESC="voles-user 2.2.2 FRG83G 91102 release-keys" BUILD_NUMBER=75603 BUILD_UTC_DATE=1289367602 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=voles BUILD_ID=FRG83G BUILD_DISPLAY_ID=GRJ22 TARGET_DEVICE=sholes BUILD_FINGERPRINT=verizon/voles/sholes/sholes:2.2.2/FRG83G/91102:user/release-keys PRODUCT_BRAND=verizon PRIVATE_BUILD_DESC="voles-user 2.2.2 FRG83G 91102 release-keys" BUILD_NUMBER=91102 BUILD_UTC_DATE=1294972140 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
 
 # Copy sholes specific prebuilt files
 PRODUCT_COPY_FILES +=  \
