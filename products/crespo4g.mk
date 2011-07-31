1527c211864d2a6414f335cf16f0f93dca4c0399
Peter Alfonso <peteralfonsojr@gmail.com>
2011-07-31 15:01:39 -0400

    Revert "Remove BUILD_DISPLAY_ID"
    This reverts commit 17a7ed2046268e3e181f97f5615ce9bab35507b1.

--- products/crespo4g.mk
+++ products/crespo4g.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/pete/products/common.mk)
 # Setup device specific product configuration.
 PRODUCT_NAME := pete_crespo4g
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=sojus BUILD_ID=GRJ90 BUILD_FINGERPRINT=google/sojus/crespo4g:2.3.5/GRJ90/138666:user/release-keys PRIVATE_BUILD_DESC="sojus-user 2.3.5 GRJ90 138666 release-keys" BUILD_NUMBER=138666 BUILD_UTC_DATE=1309378584 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=sojus BUILD_ID=GRJ90 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=google/sojus/crespo4g:2.3.5/GRJ90/138666:user/release-keys PRIVATE_BUILD_DESC="sojus-user 2.3.5 GRJ90 138666 release-keys" BUILD_NUMBER=138666 BUILD_UTC_DATE=1309378584 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
 
 # Copy crespo4g specific prebuilt files
 PRODUCT_COPY_FILES +=  \
