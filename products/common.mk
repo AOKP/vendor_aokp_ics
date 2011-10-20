4118c89adfc7ae3aeffafaebf7fd5b5044ba3a97
Peter Alfonso <peteralfonsojr@gmail.com>
2011-10-20 19:45:54 -0400

    Add more common properties

--- products/common.mk
+++ products/common.mk
@@ -17,7 +17,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.error.receiver.system.apps=com.google.android.feedback \
+    ro.com.google.locationfeatures=1 \
     ro.setupwizard.mode=OPTIONAL \
+    ro.setupwizard.enable_bypass=1 \
     ro.setupwizard.enterprise_mode=1
 
 # ROM specific product packages
