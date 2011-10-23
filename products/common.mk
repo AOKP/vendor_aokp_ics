14f899fbf44d478e7227943e1f6e698ce3c5eada
Peter Alfonso <peteralfonsojr@gmail.com>
2011-10-23 18:50:43 -0400

    Remove redundant setupwizard property

--- products/common.mk
+++ products/common.mk
@@ -17,7 +17,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.error.receiver.system.apps=com.google.android.feedback \
     ro.com.google.locationfeatures=1 \
     ro.setupwizard.mode=OPTIONAL \
-    ro.setupwizard.enable_bypass=1 \
     ro.setupwizard.enterprise_mode=1
 
 # ROM specific product packages
