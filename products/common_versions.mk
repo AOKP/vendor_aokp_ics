7ab8225d2e41481fad82c980ac7fce5a6012aec8
Peter Alfonso <peteralfonsojr@gmail.com>
2011-12-06 20:23:14 -0500

    Add Rom Manager properties

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,1 +1,8 @@
+# Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=GPA19 BUILD_VERSION_TAGS=release-keys USER=android-build
+
+# Rom Manager properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.rommanager.developerid=pete \
+    ro.modversion=GPA19
+
