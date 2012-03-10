3cebc18c77d92405c4d48ba4dc9442ebab1edbf9
netchip <netchip@netchip-ubuntu.(none)>
2012-03-10 19:23:02 +0100

    galaxys2

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,6 +5,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
+    $(LOCAL_DIR)/galaxys2.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
     $(LOCAL_DIR)/otter.mk \
     $(LOCAL_DIR)/p4.mk \
---
+++ products/galaxys2.mk
@@ -0,0 +1,26 @@
+$(call inherit-product, device/samsung/galaxys2/full_galaxys2.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := GalaxyS2
+
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/galaxys2
+
+# $(call inherit-product, vendor/cm/config/gsm.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := galaxys2
+PRODUCT_NAME := aokp_galaxys2
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-I9100
+
+#Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.0.3/IML74K/XXLPB:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 4.0.3 IML74K XXLPB release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+	vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
