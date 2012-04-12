909f301e4e598e6acfd87ec479204894c27a0e46
Steve Spear <stephen.k.spear@gmail.com>
2012-04-11 22:58:10 -0600

    Trying mesmerize on stock MMS again

--- products/mesmerizemtd.mk
+++ products/mesmerizemtd.mk
@@ -18,7 +18,6 @@ PRODUCT_MODEL := SCH-I500
 #Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SCH-I500 TARGET_DEVICE=SCH-I500 BUILD_ID=IML74K BUILD_FINGERPRINT=google/soju/crespo:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.4 GRJ22 121341 release-keys"
 
-# boot animation/ Modded Mms.apk
+# boot animation
 PRODUCT_COPY_FILES += \
-        vendor/aokp/prebuilt/mesmerizemtd/app/Mms.apk:system/app/Mms.apk \
 	vendor/aokp/prebuilt/mesmerizemtd/media/bootanimation.zip:system/media/bootanimation.zip
