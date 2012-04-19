97efbd946a61157f023708fd6a56d42f3111d1cd
Steve Spear <stephen.k.spear@gmail.com>
2012-04-18 21:34:55 -0600

    Add boot_audio

--- products/fascinatemtd.mk
+++ products/fascinatemtd.mk
@@ -20,4 +20,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SCH-I500 TARGET_DEVICE=SCH-I500 BUI
 
 # boot animation
 PRODUCT_COPY_FILES += \
+	vendor/aokp/prebuilt/mesmerizemtd/media/boot_audio.mp3:system/media/boot_audio.mp3 \
 	vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
