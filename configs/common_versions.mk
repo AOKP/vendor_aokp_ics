79eb3bd0efb58bc90002f86bf004be04c25a9a47
Steve Spear <stephen.k.spear@gmail.com>
2012-04-26 09:06:15 -0600

    Build ID bump

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,5 +1,5 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76D BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 CURRENT_VERSION=$(TARGET_PRODUCT)_Milestone-5-Glitched
 
