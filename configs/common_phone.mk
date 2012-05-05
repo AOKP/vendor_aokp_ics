c329998cbb2f39e96366eb814be7b645c5eeff1d
Steve Spear <stephen.k.spear@gmail.com>
2012-05-04 21:32:22 -0600

    Add modified Nova.

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,7 +1,8 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/app/ApexLauncher.apk:system/app/ApexLauncher.apk
+    vendor/aokp/prebuilt/common/app/ApexLauncher.apk:system/app/ApexLauncher.apk \
+    vendor/aokp/prebuilt/common/app/NovaLauncher_1.0.3beta2.apk:system/app/NovaLauncher_1.0.3beta2.apk
      
     
     
