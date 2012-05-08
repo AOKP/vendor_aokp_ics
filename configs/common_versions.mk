d6e25dc67e398c042e08a924514936cea9bb931f
Steve Spear <stephen.k.spear@gmail.com>
2012-05-07 21:39:06 -0600

    themes and Hybrid stuff

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,30 +1,18 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-CURRENT_VERSION=$(TARGET_PRODUCT)_Build-35-Glitched
-
-ifneq ($(TARGET_PRODUCT),aokp_p999)
-    ifeq ($(TARGET_PRODUCT),aokp_p990)
-	    PRODUCT_PROPERTY_OVERRIDES += \
-	        ro.cm.version=9.0.0-$(CURRENT_VERSION)
-     else
-         PRODUCT_PROPERTY_OVERRIDES += \
-	        ro.modversion=$(CURRENT_VERSION)
-    endif
-else
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.cm.version=9.0.0-$(CURRENT_VERSION)
-endif
-
+# Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=TeamGlitch
+    ro.rommanager.developerid=teamkang \
+    ro.aokp.version=$(TARGET_PRODUCT)_Build-35-Glitched
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aokp \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=13
+    ro.goo.version=12
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.sys.camera-sound=1
+
