8740d8fe80383addef1975da84a0858874ac8244
Steve Spear <stephen.k.spear@gmail.com>
2012-05-08 08:42:31 -0600

    update apex, nova, squisher

--- configs/common.mk
+++ configs/common.mk
@@ -31,7 +31,6 @@ PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf  \
     vendor/aokp/prebuilt/common/app/Microbes.apk:system/app/Microbes.apk \
     vendor/aokp/prebuilt/common/app/TitaniumBackup_latest.apk:system/app/TitaniumBackup_latest.apk \
-    vendor/aokp/prebuilt/common/app/mobi.cyann.nstools-2.apk:system/app/mobi.cyann.nstools-2.apk \
     vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so
     
 # init.d
--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -2,7 +2,7 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/app/ApexLauncher.apk:system/app/ApexLauncher.apk \
-    vendor/aokp/prebuilt/common/app/NovaLauncher_1.0.3beta2.apk:system/app/NovaLauncher_1.0.3beta2.apk
+    vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
      
     
     
