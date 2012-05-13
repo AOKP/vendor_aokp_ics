37dfa96a2d79115a967755f25f4993e0a2aad5a1
Steve Spear <stephen.k.spear@gmail.com>
2012-05-12 18:11:33 -0600

    Rookie mistake you hate to see it

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -3,6 +3,3 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/app/ApexLauncher.apk:system/app/ApexLauncher.apk \
     vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
-     
-    
-    
