b8156a3815d716a9f8e205b568a153ac2b2932da
Steve Spear <stephen.k.spear@gmail.com>
2012-05-14 17:27:41 -0600

    merge upstream

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,15 +4,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=relea
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_build-35-Glitched
+    ro.aokp.version=$(TARGET_PRODUCT)_build-3-Glitched
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aokp \
     ro.goo.rom=aokp \
-    ro.goo.version=13
-
-# Camera shutter sound property
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.camera-sound=1
-
+    ro.goo.version=14
