509c07b064fc1af91d66ca42a7ec5e4d33da093a
Steve Spear <stephen.k.spear@gmail.com>
2012-05-18 21:45:53 -0600

    Add TouchWiz for a build J4F

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -2,6 +2,8 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/app/ApexLauncher.apk:system/app/ApexLauncher.apk \
+    vendor/aokp/prebuilt/common/app/TouchWiz.apk:system/app/TouchWiz.apk \
+    vendor/aokp/prebuilt/common/lib/libglcanvas.so:system/lib/libglcanvas.so \
     vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
      
     
