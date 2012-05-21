0dc3d2ff890ee44c753ace9527ad54e5ec9c3adb
Steve Spear <stephen.k.spear@gmail.com>
2012-05-21 09:58:16 -0600

    Take S voice out for now

--- configs/samsung.mk
+++ configs/samsung.mk
@@ -34,23 +34,11 @@ PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/app/Microbes.apk:system/app/Microbes.apk \
     vendor/aokp/prebuilt/common/app/TitaniumBackup.apk:system/app/TitaniumBackup.apk \
     vendor/aokp/prebuilt/common/app/NSTools.apk:system/app/NSTools.apk \
-    vendor/aokp/prebuilt/common/app/voicetalk.apk:system/app/voicetalk.apk \
     vendor/aokp/prebuilt/common/app/UnicornPorn.apk:system/app/UnicornPorn.apk \
     vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so
     
 # Apps/Libs for SGS3
 PRODUCT_COPY_FILES += \
-
-    vendor/aokp/prebuilt/samsung/app/voicetalk.apk:system/app/voicetalk.apk \
-    vendor/aokp/prebuilt/samsung/lib/libogg.so:system/lib/libogg.so \
-    vendor/aokp/prebuilt/samsung/lib/libsamsungRecord.so:system/lib/libsamsungRecord.so \
-    vendor/aokp/prebuilt/samsung/lib/libsamsungtts.so:system/lib/libsamsungtts.so \
-    vendor/aokp/prebuilt/samsung/lib/libSamsungVoiceEngine.so:system/lib/libSamsungVoiceEngine.so \
-    vendor/aokp/prebuilt/samsung/lib/libsensory.so:system/lib/libsensory.so \
-    vendor/aokp/prebuilt/samsung/lib/libspeex.so:system/lib/libspeex.so \
-    vendor/aokp/prebuilt/samsung/lib/libspeexJNI.so:system/lib/libspeexJNI.so \
-    vendor/aokp/prebuilt/samsung/lib/libsvoxtts.so:system/lib/libsvoxtts.so \
-    vendor/aokp/prebuilt/samsung/lib/libVLSilJNI.so:system/lib/libVLSilJNI.so \
     
 # init.d
 PRODUCT_COPY_FILES += \
