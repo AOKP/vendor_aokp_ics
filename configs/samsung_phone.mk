070e07b134deee34ce4ef3b0fd3152d9d8ada3ca
Steve Spear <stephen.k.spear@gmail.com>
2012-05-21 16:50:16 -0600

    derp

--- configs/samsung_phone.mk
+++ configs/samsung_phone.mk
@@ -6,7 +6,7 @@ PRODUCT_COPY_FILES += \
 
 # Apps/Libs for SGS3
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/app/voicetalk.apk:system/app/voicetalk.apk \
+    vendor/aokp/prebuilt/samsung/app/voicetalk.apk:system/app/voicetalk.apk \
     vendor/aokp/prebuilt/samsung/lib/libogg.so:system/lib/libogg.so \
     vendor/aokp/prebuilt/samsung/lib/libsamsungRecord.so:system/lib/libsamsungRecord.so \
     vendor/aokp/prebuilt/samsung/lib/libsamsungtts.so:system/lib/libsamsungtts.so \
