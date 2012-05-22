0cee96dd4574f2102851ea0cd9b123923748e798
Steve Spear <stephen.k.spear@gmail.com>
2012-05-21 22:22:16 -0600

    Might have gotten ambitious

--- configs/samsung_phone.mk
+++ configs/samsung_phone.mk
@@ -7,12 +7,6 @@ PRODUCT_COPY_FILES += \
 # Apps/Libs for SGS3
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/samsung/app/voicetalk.apk:system/app/voicetalk.apk \
-    vendor/aokp/prebuilt/samsung/lib/libogg.so:system/lib/libogg.so \
     vendor/aokp/prebuilt/samsung/lib/libsamsungRecord.so:system/lib/libsamsungRecord.so \
     vendor/aokp/prebuilt/samsung/lib/libsamsungtts.so:system/lib/libsamsungtts.so \
     vendor/aokp/prebuilt/samsung/lib/libSamsungVoiceEngine.so:system/lib/libSamsungVoiceEngine.so \
-    vendor/aokp/prebuilt/samsung/lib/libsensory.so:system/lib/libsensory.so \
-    vendor/aokp/prebuilt/samsung/lib/libspeex.so:system/lib/libspeex.so \
-    vendor/aokp/prebuilt/samsung/lib/libSpeexJNI.so:system/lib/libSpeexJNI.so \
-    vendor/aokp/prebuilt/samsung/lib/libsvoxtts.so:system/lib/libsvoxtts.so \
-    vendor/aokp/prebuilt/samsung/lib/libVLSilJNI.so:system/lib/libVLSilJNI.so
