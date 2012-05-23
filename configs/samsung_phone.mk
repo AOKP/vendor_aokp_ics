49db71c5e77d453655968c718f454626dd49b449
Steve Spear <stephen.k.spear@gmail.com>
2012-05-23 07:21:06 -0600

    Ditch s voice and trebuchet

--- configs/samsung_phone.mk
+++ configs/samsung_phone.mk
@@ -2,10 +2,3 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
-
-# Apps/Libs for SGS3
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/samsung/app/voicetalk.apk:system/app/voicetalk.apk
-    
-PRODUCT_PACKAGES += \
-        Trebuchet
