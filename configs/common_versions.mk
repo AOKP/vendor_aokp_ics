017d0d6be8aa87e9fe08453d238bf9f914f386cc
Steve Spear <stephen.k.spear@gmail.com>
2012-06-27 19:20:10 -0600

    Fix up otter

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,10 +4,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=relea
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
-    ro.aokp.version=$(TARGET_PRODUCT)_build-40-Devil
+    ro.aokp.version=$(TARGET_PRODUCT)_Milestone-6-Devil
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aokp \
     ro.goo.rom=aokp \
-    ro.goo.version=18
+    ro.goo.version=19
